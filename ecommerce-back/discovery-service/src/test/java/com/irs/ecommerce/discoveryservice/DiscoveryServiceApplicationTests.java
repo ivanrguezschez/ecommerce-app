c25a11cc6883ffe170933483cf630fd47b6a2636
Iván Rodríguez Sánchez <ivan.rguez.schez@gmail.com>
2025-04-23 22:44:01 +0200

    Se añade el microservicio de registro de servicios eureka

---
+++ ecommerce-back/discovery-service/src/test/java/com/irs/ecommerce/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irs.ecommerce.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
