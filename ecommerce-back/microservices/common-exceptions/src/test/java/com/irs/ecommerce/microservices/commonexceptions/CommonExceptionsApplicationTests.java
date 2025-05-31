76122c086c3d48c0c5729b7215578a073289ace9
Iván Rodríguez Sánchez <ivan.rguez.schez@gmail.com>
2025-05-31 21:01:14 +0200

    Se refactoriza el directorio de test del módulo common-exceptions

--- ecommerce-back/microservices/common-exceptions/src/test/java/com/irs/ecommerce/microservices/commonexceptions/CommonExceptionsApplicationTests.java
+++ ecommerce-back/microservices/common-exceptions/src/test/java/com/irs/ecommerce/microservices/commonexceptions/CommonExceptionsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.irs.ecommerce.microservices.customerservice;
+package com.irs.ecommerce.microservices.commonexceptions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerServiceApplicationTests {
+class CommonExceptionsApplicationTests {
 
 	@Test
 	void contextLoads() {
